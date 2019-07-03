3131dbfdae99eaa066ef1abab7e49effdc729166
Morgan Stanley <morganpstanley@gmail.com>
2019-07-03 23:04:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+require 'pry'
+
+def line(current_line)
+  if current_line == []
+    current_line_result = "The line is currently empty."
+  else
+    i = 1
+    current_line_result = "The line is currently:"
+    current_line.each do |person|
+      current_line_result << " #{i}. #{person}"
+      i += 1
+    end
+  end
+  puts current_line_result
+end
+
+def take_a_number(current_line, new_person)
+  current_line.push(new_person)
+  puts "Welcome, #{new_person}. You are number #{current_line.length} in line."
+end
+
+def now_serving(current_line)
+  if current_line == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{current_line.shift}."
+  end
+end
